5b8920827510c1b32fed9bdca4718fd7b29ce36e
Gangadhar <you@example.com>
2026-06-14 17:44:22 +0530

    add

--- testcases/conftest.py
+++ testcases/conftest.py
@@ -2,11 +2,17 @@ from time import sleep
 import pytest
 from selenium.webdriver import Chrome,ChromeOptions
 from utilites.verification import *
+from POM.login import Login
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 base=r"https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
 @pytest.fixture
 def launch():
     opts=ChromeOptions()
-    opts.add_experimental_option("detach",True)
+    # opts.add_experimental_option("detach",True)
+    opts.add_argument("--start-maximized")
+    opts.add_argument("--disable-dev-shm-usage")
+    opts.add_argument("--no-sandbox")
     driver=Chrome(opts)
     driver.get(base)
     driver.maximize_window()
